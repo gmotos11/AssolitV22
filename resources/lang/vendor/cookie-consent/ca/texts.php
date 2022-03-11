e40115f79ff7a4e988a414dad24a1790066315e3
gmotos11 <mcapel@insdanielblanxart.cat>
2022-03-11 12:19:41 +0100

    Cookies traducidas 11/03/22

--- resources/lang/vendor/cookie-consent/ca/texts.php
+++ resources/lang/vendor/cookie-consent/ca/texts.php
@@ -4,5 +4,56 @@ return [
     'privacy' => 'Llegeix sobre la nostra Politica de Privacitat...',
     'msg' => "La web d'Assolit utilitza cookies pròpies i de tercers per a finalitats analítiques, recopilant informació estadística de l'ús de la pàgina web. Pots acceptar totes les cookies o rebutjar-ne l'ús",
     'agr' => 'Acceptar cookies',
-    'deny' => 'Rebutjar cookies'
+    'deny' => 'Rebutjar cookies',
+    'msg_privacy' => "
+    En aquesta Web es poden estar utilitzant cookies en algunes pàgines. La finalitat d’aquestes cookies és millorar el servei que la nostra organització ofereix als seus clients i als nostres visitants.
+Les cookies són petits fitxers de dades que es generen en l’ordinador de l’usuari i que permeten obtenir la següent informació:<br><br>
+    <ul>
+        <li>Data i hora de l'última vegada que l'usuari va visitar el Web.</li>
+        <li>Disseny de contingut que l'usuari va triar en la seva primera visita al Web.</li>
+        <li>Elements de seguretat que intervenen en el control d'accés a les àrees restringides.</li>
+    </ul>
+
+ASSOLIT SL posa en coneixement dels seus usuaris que independentment de les dades de caràcter personal que obté dels usuaris registrats i els que contracten els seus serveis, ASSOLIT SL, obté i conserva la següent informació:<br><br>
+<ul>
+    <li>Nombre de visitants.</li>
+    <li>Nom de domini del proveïdor que els dóna accés a la xarxa.</li>
+    <li>Data i hora d'accés al lloc web.</li>
+    <li>Adreça d'Internet des de la qual parteix el link que dirigeix a l'usuari al nostre lloc web.</li>
+    <li>Recorregut i clics fets per l'usuari al llarg de la Web i enllaços clicats.
+    </li>
+</ul>
+
+El nostre programari i l’analitzador de trànsit del site utilitza cookies i seguiments de IPs que ens permeten recollir dades a efectes estadístics com: data de la primera visita, número a vegades que s’ha visitat, data de l’última visita, URL i domini de la qual prové, explorador utilitzat i resolució de pantalla.<br><br> No obstant això, l’usuari si ho desitja pot desactivar i/o eliminar aquestes cookies seguint les instruccions del seu navegador d’Internet a pesar que la seva capacitat d’utilitzar funcionalitats del site pot veure’s minvada sensiblement i no ser compatible amb les necessitats de l’usuari.
+<br><br>Aquest lloc web usa cookies d’anàlisis que permeten quantificar el nombre d’usuaris del lloc web i realitzar la mesura i anàlisi estadística de l’ús del web, amb l’objectiu de millorar l’oferta de continguts i serveis.<br><br> ASSOLIT SL utilitza el servei de Google, Google Analytics, que identifica de manera anònima l’usuari, si és nou o repeteix visita, el temps que ha passat en el web, i comptabilitza el nombre de visitants i els continguts més visitats.
+<br><br>
+Les Cookies de Google Analytics utilitzades en aquest web són:<br><br>
+<ul>
+    <li>_utma</li>
+    <li>_utmb</li>
+    <li>_utmc</li>
+    <li>_utmv</li>
+    <li>_utmz</li>
+</ul>
+L’usuari pot inhabilitar en qualsevol moment l’ús de la cookie de Google a través de la desactivació de la cookie del seu navegador.
+<br><br>La següent taula mostra les cookies pròpies i de tercers que utilitzem en la web i la finalitat per a la qual s’utilitza cadascuna.<br><br>
+
+<table class='tftable' border='1'>
+    <tr><th>Nom</th><th>Propietat</th><th>Duració</th><th>Descripció</th></tr>
+    <tr><td>_gdpr_popup</td><td>Propia</td><td>1 any</td><td>Utilitzada per a guardar les preferències sobre cookies</td></tr>
+    <tr><td>CONSENT</td><td>Cookies google.com</td><td>Permanent</td><td>Cookie tècnica per a controlar l’acceptació de cookies.</td></tr>
+    <tr><td>SEARCH_SAMESITE</td><td>Google Analytics</td><td>6 mesos</td><td>Aquesta cookie serveix per al correcte enviament de dades a google.</td></tr>
+    <tr><td>_ga</td><td>Google Analytics</td><td>2 anys</td><td>S’utilitza per a distingir als usuaris.</td></tr>
+    <tr><td>_gid</td><td>Google Analytics</td><td>24 hores</td><td>S’utilitza per a distingir als usuaris.</td></tr>
+    <tr><td>_gat_gtag</td><td>Google Analytics</td><td>Sessió</td><td>S’utilitza per a limitar el percentatge de sol·licituds. Si s’ha implementat Google Analytics mitjançant Google Tag Manager, aquesta cookie es dirà _dc_gtm_.</td></tr>
+    <tr><td>ANID</td><td>Cookies google.com</td><td>Sessió</td><td>La cookie “ANID” s’utilitza per a la publicitat que es publica en el web i s’emmagatzema en google.com.</td></tr>
+    <tr><td>1P_JAR</td><td>Cookies google.com</td><td>1 mes</td><td>Per a personalitzar els anuncis segons els interessos de l’usuari.</td></tr>
+    <tr><td>SIDCC</td><td>Cookies google.com</td><td>1 any</td><td>Per a proveir serveis i extreure informació anònima sobre la navegació.</td></tr>
+    <tr><td>SID, SAPISID, APISID</td><td>Google Maps</td><td>2 anys</td><td>Són utilitzades per Google per a emmagatzemar les preferències de l’usuari i la informació durant la visualització de les pàgines amb els mapes de Google en ells.</td></tr>
+    <tr><td>SSID, HSID, NID, OTZ</td><td>Google Maps</td><td>6 mesos</td><td>Per al recompte, per part de Google, del número d’usuari que utilitzen els mapes.</td></tr>
+    <tr><td>__Secure-3PSIDCC, __Secure-3PSID, __Secure-1PSID, __Secure-3PAPISID, __Secure-1PAPISID</td><td>Google.com</td><td>2 anys</td><td>Utilitzat per amb finalitats d’orientació per a crear un perfil dels interessos dels visitants del lloc web amb la finalitat de mostrar publicitat de Google rellevant i personalitzada.</td></tr>
+    <tr><td>_GRECAPTCHA</td><td>Google reCAPTCHA</td><td>Sessió</td><td>Proporcionar protecció contra correu no desitjat</td></tr>
+</table>
+    
+    "
 ];
--- resources/lang/vendor/cookie-consent/es/texts.php
+++ resources/lang/vendor/cookie-consent/es/texts.php
@@ -34,31 +34,23 @@ return [
     </ul>
 
     El usuario puede inhabilitar en cualquier momento el uso de la cookie de Google a través de la desactivación de la cookie de su navegador.
-    La siguiente tabla muestra las cookies propias y de terceros que utilizamos en la web y la finalidad para la que se utiliza cada una. <br><br>
-    <style type="text/css">
-.tftable {font-size:12px;color:#333333;width:100%;border-width: 1px;border-color: #a9a9a9;border-collapse: collapse;}
-.tftable th {font-size:12px;background-color:#b8b8b8;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;text-align:left;}
-.tftable tr {background-color:#cdcdcd;}
-.tftable td {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;}
-.tftable tr:hover {background-color:#ffffff;}
-</style>
+    <br><br>La siguiente tabla muestra las cookies propias y de terceros que utilizamos en la web y la finalidad para la que se utiliza cada una. <br><br>
 
 <table class="tftable" border="1">
     <tr><th>Nombre</th><th>Propiedad</th><th>Duración</th><th>Descripción</th></tr>
-    <tr><td>_gdpr_popup</td><td>Propia</td><td>1 año</td><td>_gdpr_popup	Propia	1 año	Utilizada para guardar las preferencias sobre cookies.</td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
-    <tr><td> </td><td> </td><td> </td><td> </td></tr>
+    <tr><td>_gdpr_popup</td><td>Propia</td><td>1 año</td><td>Utilizada para guardar las preferencias sobre cookies.</td></tr>
+    <tr><td>CONSENT</td><td>Cookies google.com</td><td>Permanente</td><td>Cookie técnica para controlar la aceptación de cookies.</td></tr>
+    <tr><td>SEARCH_SAMESITE</td><td>Google Analytics</td><td>6 meses</td><td>Esta cookie sirve para el correcto envío de datos a google.</td></tr>
+    <tr><td>_ga</td><td>Google Analytics</td><td>2 años</td><td>Se usa para distinguir a los usuarios.</td></tr>
+    <tr><td>_gid</td><td>Google Analytics</td><td>24 horas</td><td>Se usa para distinguir a los usuarios.</td></tr>
+    <tr><td>_gat_gtag</td><td>Google Analytics</td><td>Sesión</td><td>Se usa para limitar el porcentaje de solicitudes. Si se ha implementado Google Analytics mediante Google Tag Manager, esta cookie se llamará _dc_gtm_.</td></tr>
+    <tr><td>ANID</td><td>Cookies google.com</td><td>Sesión</td><td>ANID	Cookies google.com	Sesión	La cookie «ANID» se utiliza para la publicidad que se publica en la web y se almacena en google.com.</td></tr>
+    <tr><td>1P_JAR</td><td>Cookies google.com</td><td>1 mes</td><td>Para personalizar los anuncios según los intereses del usuario.</td></tr>
+    <tr><td>SIDCC</td><td>Cookies google.com</td><td>1 año</td><td>Para proveer servicios y extraer información anónima sobre la navegación.</td></tr>
+    <tr><td>SID, SAPISID, APISID</td><td>Google Maps</td><td>2 años</td><td>Son utilizadas por Google para almacenar las preferencias del usuario y la información durante la visualización de las páginas con los mapas de Google en ellos.</td></tr>
+    <tr><td>SSID, HSID, NID, OTZ</td><td>Google Maps</td><td>6 meses</td><td>Para el recuento, por parte de Google, del número de usuario que utilizan los mapas.</td></tr>
+    <tr><td>__Secure-3PSIDCC, __Secure-3PSID, __Secure-1PSID, __Secure-3PAPISID, __Secure-1PAPISID</td><td>Google.com</td><td>2 años</td><td>Utilizado por con fines de orientación para crear un perfil de los intereses de los visitantes del sitio web con el fin de mostrar publicidad de Google relevante y personalizada.</td></tr>
+    <tr><td>_GRECAPTCHA</td><td>Google reCAPTCHA</td><td>Sesión</td><td>Proporcionar protección contra correo no deseado.</td></tr>
 </table>
-
-
     '
 ];
