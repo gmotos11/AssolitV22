8170133f8cbde87a59dc7ae07c71abb76b256e37
gmotos11 <91206144+gmotos11@users.noreply.github.com>
2022-04-01 12:57:26 +0200

    CON HEADER Y AÑADIR QUENES SOMOS

--- resources/lang/en/multi_leng.php
+++ resources/lang/en/multi_leng.php
@@ -17,5 +17,15 @@ return [
        'idioma_img_CA'   => 'assets/img/leng/lengCA.png',
        'idioma_img_ES'   => 'assets/img/leng/lengES.jpg',
        'idioma_img_EN'   => 'assets/img/leng/lengEN.png',
+
+          //QUIENES SOMOS
+    'QS_Title'   => 'Somos tu socio de confianza en la integración de sistemas de automatización de edificios',
+    'QS_firstP'   => 'Estamos especializados en el diseño, programación, integración, mantenimiento y evolución de arquitecturas BMS (Building Managment Systems) en edificios principalmente del sector terciario.',
+    'QS_secondP'   => 'Contamos con más de 20 años de experiencia participando en proyectos de automatización y control de edificios en diversos sectores y entornos: comercio retail, centros de datos, sanidad, administraciones públicas, centros educativos, hospitalidad y acomodación, oficinas de negocios, residencial, transporte, etc.',
+    'QS_FirstTick'   => 'Aportamos servicio a medida a las necesidades de cada cliente. Cada entorno es diferente y cada aplicación del control y automatización de instalaciones de edificio es particular a las necesidades de negocio de cada organización.',
+    'QS_SecondTick'   => 'Estamos enfocados a la entrega de resultados y a la optimización continua de los mismos. Nuestra misión es generar eficiencia, control y versatilidad en la automatización de edificios procurando siempre obtener el mejor equilibrio entre el control y la mejora funcional y de confort de los usuarios de cada espacio. ',
+    'QS_ThirdTick'   => '	Proveemos de herramientas de la gestión simplificadas aportando de los equipos de mantenimiento de cada organización agilidad en la asimilación y operación de los sistemas sin tener que hacer frente a una compleja curva de aprendizaje. ',
+    'QS_LastP'   => 'Trabajamos con los fabricantes lideres del mercado. Además, somos socios certificados de las soluciones “EcoXpert” de Schneider Electric”, uno de los jugadores tecnológicos del mercado con una visión más holística, madura e innovadora del sector de la inmótica. ',
+
 ];
 ?>
