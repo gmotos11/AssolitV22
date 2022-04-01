8144c2ed475dbd9c4f8e622923fcb1bcab3469c6
gmotos11 <mcapel@insdanielblanxart.cat>
2022-04-01 10:54:51 +0200

    cookie en hecho
    cookies

--- resources/lang/vendor/cookie-consent/en/texts.php
+++ resources/lang/vendor/cookie-consent/en/texts.php
@@ -5,26 +5,26 @@ return [
     'msg' => 'The Assolit S.L. website uses its own and third-party cookies for analytical purposes, compiling statistical information on the use of the website. You can accept all cookies or reject their use.',
     'agr' => 'Allow cookies',
     'deny' => 'Deny cookies',
-    'msg_privacy' => 'En esta Web se pueden estar utilizando cookies en algunas páginas. La finalidad de estas cookies es mejorar el servicio que nuestra organización ofrece a sus clientes y a nuestros visitantes.
-    Las cookies son pequeños ficheros de datos que se generan en el ordenador del usuario y que permiten obtener la siguiente información:<br><br>
+    'msg_privacy' => "This website may be using cookies on some pages. The purpose of these cookies is to improve the service that our organization offers its customers and our visitors.
+    Cookies are small data files that are generated on the users computer and that allow the following information to be obtained:<br><br>
         <ul>
-            <li>Fecha y hora de la última vez que el usuario visitó la Web.</li>
-            <li>Diseño de contenido que el usuario escogió en su primera visita a la Web.</li>
-            <li>Elementos de seguridad que intervienen en el control de acceso a las áreas restringidas.</li>
+            <li>Date and time of the last time the user visited the Web.</li>
+            <li>Content design that the user chose on his first visit to the Web.</li>
+            <li>Security elements involved in controlling access to restricted areas.</li>
         </ul>
     
-    ASSOLIT SL pone en conocimiento de sus usuarios que independientemente de los datos de carácter personal que obtiene de los usuarios registrados y los que contratan sus servicios, ASSOLIT SL, obtiene y conserva la siguiente información:<br><br>
+        ASSOLIT SL informs its users that regardless of the personal data it obtains from registered users and those who contract its services, ASSOLIT SL obtains and keeps the following information:<br><br>
     <ul>
-        <li>Número de visitantes.</li>
-        <li>Nombre de dominio del proveedor que les da acceso a la red.</li>
-        <li>Fecha y hora de acceso al sitio web.</li>
-        <li>Dirección de Internet desde la cual parte el link que dirige al usuario a nuestro sitio web.</li>
-        <li>Recorrido y clics hechos por el usuario a lo largo de la Web y enlaces clicados.</li>
+        <li>Number of visitors.</li>
+        <li>Domain name of the provider that gives them access to the network.</li>
+        <li>Date and time of access to the website.</li>
+        <li>Internet address from which the link that directs the user to our website starts.</li>
+        <li>Route and clicks made by the user throughout the Web and clicked links.</li>
     </ul>
-    Nuestro software y el analizador de tráfico del site utiliza cookies y seguimientos de IPs que nos permiten recoger datos a efectos estadísticos cómo: fecha de la primera visita, número a veces que se ha visitado, fecha de la última visita, URL y dominio de la cual proviene, explorador utilizado y resolución de pantalla.<br><br> Sin embargo, el usuario si lo desea puede desactivar y/o eliminar estas cookies siguiendo las instrucciones de su navegador de Internet a pesar de que su capacidad de utilizar funcionalidades del site puede verse menguada sensiblemente y no ser compatible con las necesidades del usuario.
-    <br><br>Este sitio web usa cookies de análisis que permiten cuantificar el número de usuarios del sitio web y realizar la medida y análisis estadístico del uso del web, con el objetivo de mejorar la oferta de contenidos y servicios.<br><br>ASSOLIT SL usa el servicio de Google, Google Analytics, que identifica de forma anónima el usuario, si es nuevo o repite visita, el tiempo que ha pasado en el web, y contabiliza el número de visitantes y los contenidos más visitados.
+    Our software and site traffic analyzer use cookies and IP tracking that allow us to collect data for statistical purposes such as: date of first visit, number of times visited, date of last visit, URL and domain from which it comes, browser used and screen resolution. However, the user may, if they wish, deactivate and / or delete these cookies following the instructions of their Internet browser, although their ability to use the site's functionalities may be significantly reduced and may not be compatible with their needs of the user.
+    <br><br>This website uses analysis cookies that allow us to quantify the number of users of the website and to carry out the measurement and statistical analysis of the use of the website, with the aim of improving the offer of content and services. ASSOLIT SL uses the Google service, Google Analytics, which anonymously identifies the user, if he is new or repeats a visit, the time spent on the web, and counts the number of visitors and the most visited content.
     <br><br>
-    Las Cookies de Google Analytics utilizadas en esta web son:<br><br>
+    The Google Analytics Cookies used on this website are:<br><br>
     <ul>
         <li>_utma</li>
         <li>_utmb</li>
@@ -33,18 +33,18 @@ return [
         <li>_utmz</li>
     </ul>
 
-    El usuario puede inhabilitar en cualquier momento el uso de la cookie de Google a través de la desactivación de la cookie de su navegador.
-    <br><br>La siguiente tabla muestra las cookies propias y de terceros que utilizamos en la web y la finalidad para la que se utiliza cada una. <br><br>
+    You can disable the use of the Google cookie at any time by disabling the cookie in your browser.
+    <br><br>The following table shows our own and third-party cookies that we use on the website and the purpose for which each one is used. <br><br>
 
-<table class="tftable" border="1">
+<table class='tftable' border='1'>
     <tr><th>Name</th><th>Properties</th><th>Duration</th><th>Description</th></tr>
     <tr><td>_gdpr_popup</td><td>Own cookie</td><td>1 year</td><td>Used to save cookie preferences.</td></tr>
     <tr><td>CONSENT</td><td>Cookies google.com</td><td>Permanent</td><td>Technical cookie to control the acceptance of cookies.</td></tr>
     <tr><td>SEARCH_SAMESITE</td><td>Google Analytics</td><td>6 months</td><td>This cookie is used for the correct sending of data to google.</td></tr>
     <tr><td>_ga</td><td>Google Analytics</td><td>2 years</td><td>Used to distinguish users.</td></tr>
     <tr><td>_gid</td><td>Google Analytics</td><td>24 hours</td><td>Used to distinguish users.</td></tr>
-    <tr><td>_gat_gtag</td><td>Google Analytics</td><td>Sesion</td><td>Used to limit the percentage of requests. If Google Analytics is implemented using Google Tag Manager, this cookie will be called _dc_gtm_.</td></tr>
-    <tr><td>ANID</td><td>Cookies google.com</td><td>Sesion</td><td>The "ANID" cookie is used for advertising that is posted on the web and is stored on google.com.</td></tr>
+    <tr><td>_gat_gtag</td><td>Google Analytics</td><td>Session</td><td>Used to limit the percentage of requests. If Google Analytics is implemented using Google Tag Manager, this cookie will be called _dc_gtm_.</td></tr>
+    <tr><td>ANID</td><td>Cookies google.com</td><td>Session</td><td>The 'ANID' cookie is used for advertising that is posted on the web and is stored on google.com.</td></tr>
     <tr><td>1P_JAR</td><td>Cookies google.com</td><td>1 month</td><td>To customize ads based on user interests.</td></tr>
     <tr><td>SIDCC</td><td>Cookies google.com</td><td>1 year</td><td>To provide services and extract anonymous browsing information.</td></tr>
     <tr><td>SID, SAPISID, APISID</td><td>Google Maps</td><td>2 years</td><td>They are used by Google to store user preferences and information while viewing pages with Google Maps on them.</td></tr>
@@ -52,5 +52,5 @@ return [
     <tr><td>__Secure-3PSIDCC, __Secure-3PSID, __Secure-1PSID, __Secure-3PAPISID, __Secure-1PAPISID</td><td>Google.com</td><td>2 years</td><td>Used for targeting purposes to create a profile of the interests of website visitors in order to show relevant and personalized Google advertising.</td></tr>
     <tr><td>_GRECAPTCHA</td><td>Google reCAPTCHA</td><td>Session</td><td>Provide protection against spam.</td></tr>
 </table>
-    '
+    "
 ];
